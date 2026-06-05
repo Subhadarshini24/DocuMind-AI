7ea2c043742cc53fa2ce90b551c814c6e388ce38
Subhadarshini24 <pandasubh24@gmail.com>
2026-06-05 22:20:26 +0530

    Update vector_store.py

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -1,1 +1,31 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import os
 
+
+def get_text_chunks(text):
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    chunks = text_splitter.split_text(text)
+
+    return chunks
+
+
+def create_vector_store(chunks):
+
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="gemini-embedding-001",
+        google_api_key=os.getenv("GOOGLE_API_KEY")
+    )
+
+    vector_store = FAISS.from_texts(
+        chunks,
+        embedding=embeddings
+    )
+
+    vector_store.save_local("faiss_index")
