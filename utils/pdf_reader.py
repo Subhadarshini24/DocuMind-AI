9d5d983db70a051db3976884b44ca9496dbf3bed
Subhadarshini24 <pandasubh24@gmail.com>
2026-06-05 22:21:20 +0530

    Add PDF text extraction function

--- utils/pdf_reader.py
+++ utils/pdf_reader.py
@@ -1,1 +1,15 @@
+from pypdf import PdfReader
 
+def get_pdf_text(pdf_docs):
+    text = ""
+
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+
+        for page in pdf_reader.pages:
+            page_text = page.extract_text()
+
+            if page_text:
+                text += page_text
+
+    return text
